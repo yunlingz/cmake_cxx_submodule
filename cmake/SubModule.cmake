37d04f000dd4fde99093585787089640cbd47942
chuling <meetchuling@outlook.com>
2020-02-08 16:33:10 +0800

    use abbreviations: private->PVT target->TGT dependency->DEP

--- cmake/SubModule.cmake
+++ cmake/SubModule.cmake
@@ -30,27 +30,27 @@ elseif(UNIX)
 endif()
 
 function(add_inc)
-  set(OPTIONS INNER)
-  set(ONE_VALUE_ARGS TARGET_NAME)
+  set(OPTIONS PVT)
+  set(ONE_VALUE_ARGS TGT)
   cmake_parse_arguments(ADD_INC
     "${OPTIONS}" "${ONE_VALUE_ARGS}" "" ${ARGN})
 
-  if(EXISTS ${PROJECT_SOURCE_DIR}/${ADD_INC_TARGET_NAME})
-    include_directories(${PROJECT_SOURCE_DIR}/${ADD_INC_TARGET_NAME})
-    if(NOT ADD_INC_INNER)
-      install(DIRECTORY "${PROJECT_SOURCE_DIR}/${ADD_INC_TARGET_NAME}/" # trailing '/' is significant
+  if(EXISTS ${PROJECT_SOURCE_DIR}/${ADD_INC_TGT})
+    include_directories(${PROJECT_SOURCE_DIR}/${ADD_INC_TGT})
+    if(NOT ADD_INC_PVT)
+      install(DIRECTORY "${PROJECT_SOURCE_DIR}/${ADD_INC_TGT}/" # trailing '/' is significant
         DESTINATION include)
     endif()
   else()
-    message(FATAL_ERROR "${PROJECT_SOURCE_DIR}/${ADD_INC_TARGET_NAME} not exists.")
+    message(FATAL_ERROR "${PROJECT_SOURCE_DIR}/${ADD_INC_TGT} not exists.")
   endif()
 endfunction()
 
 # add library target
 function(add_lib)
-  set(OPTIONS INNER)
-  set(ONE_VALUE_ARGS TARGET_NAME)
-  set(MULTI_VALUE_ARGS SRC LINK_TO COMPILE_FLAGS LINK_FLAGS)
+  set(OPTIONS PVT)
+  set(ONE_VALUE_ARGS TGT)
+  set(MULTI_VALUE_ARGS SRC DEP COMPILE_FLAGS LINK_FLAGS)
   cmake_parse_arguments(ADD_LIB
     "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGN})
 
@@ -62,50 +62,50 @@ function(add_lib)
     endif()
   endforeach()
 
-  if(BUILD_SHARED_LIBS AND (NOT ADD_LIB_INNER))
-    add_library(${ADD_LIB_TARGET_NAME} SHARED ${SRC_LIST})
-    set_target_properties(${ADD_LIB_TARGET_NAME} PROPERTIES
+  if(BUILD_SHARED_LIBS AND (NOT ADD_LIB_PVT))
+    add_library(${ADD_LIB_TGT} SHARED ${SRC_LIST})
+    set_target_properties(${ADD_LIB_TGT} PROPERTIES
       VERSION ${PROJECT_VERSION}
       SOVERSION ${PROJECT_VERSION_MAJOR})
   else()
-    add_library(${ADD_LIB_TARGET_NAME} STATIC ${SRC_LIST})
+    add_library(${ADD_LIB_TGT} STATIC ${SRC_LIST})
   endif()
 
-  if(ADD_LIB_LINK_TO)
-    target_link_libraries(${ADD_LIB_TARGET_NAME} ${ADD_LIB_LINK_TO})
+  if(ADD_LIB_DEP)
+    target_link_libraries(${ADD_LIB_TGT} ${ADD_LIB_DEP})
   endif()
 
   if(ADD_LIB_COMPILE_FLAGS)
     string(REPLACE ";" " " ADD_LIB_COMPILE_FLAGS "${ADD_LIB_COMPILE_FLAGS}")
-    set_target_properties(${ADD_LIB_TARGET_NAME} PROPERTIES
+    set_target_properties(${ADD_LIB_TGT} PROPERTIES
       COMPILE_FLAGS ${ADD_LIB_COMPILE_FLAGS})
   endif()
 
   if(ADD_LIB_LINK_FLAGS)
     string(REPLACE ";" " " ADD_LIB_LINK_FLAGS "${ADD_LIB_LINK_FLAGS}")
-    set_target_properties(${ADD_LIB_TARGET_NAME} PROPERTIES
+    set_target_properties(${ADD_LIB_TGT} PROPERTIES
       LINK_FLAGS ${ADD_LIB_LINK_FLAGS})
   endif()
 
-  if(NOT ADD_LIB_INNER)
-    install(TARGETS ${ADD_LIB_TARGET_NAME} LIBRARY
+  if(NOT ADD_LIB_PVT)
+    install(TARGETS ${ADD_LIB_TGT} LIBRARY
       DESTINATION lib
-      COMPONENT ${ADD_LIB_TARGET_NAME})
+      COMPONENT ${ADD_LIB_TGT})
   endif()
 
-  if(BUILD_SHARED_LIBS AND (NOT ADD_LIB_INNER))
+  if(BUILD_SHARED_LIBS AND (NOT ADD_LIB_PVT))
     configure_file(${PROJECT_SOURCE_DIR}/cmake/libtemplate.pc.in
-      lib${ADD_LIB_TARGET_NAME}.pc @ONLY)
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/lib${ADD_LIB_TARGET_NAME}.pc
+      lib${ADD_LIB_TGT}.pc @ONLY)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/lib${ADD_LIB_TGT}.pc
       DESTINATION lib/pkgconfig)
   endif()
 endfunction()
 
 # add executable target
 function(add_bin)
-  set(OPTIONS INNER)
-  set(ONE_VALUE_ARGS TARGET_NAME)
-  set(MULTI_VALUE_ARGS SRC INC LINK_TO COMPILE_FLAGS LINK_FLAGS)
+  set(OPTIONS PVT)
+  set(ONE_VALUE_ARGS TGT)
+  set(MULTI_VALUE_ARGS SRC INC DEP COMPILE_FLAGS LINK_FLAGS)
   cmake_parse_arguments(ADD_BIN
     "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}" ${ARGN})
 
@@ -117,27 +117,27 @@ function(add_bin)
     endif()
   endforeach()
 
-  add_executable(${ADD_BIN_TARGET_NAME} ${SRC_LIST})
+  add_executable(${ADD_BIN_TGT} ${SRC_LIST})
 
-  if(ADD_BIN_LINK_TO)
-    target_link_libraries(${ADD_BIN_TARGET_NAME} ${ADD_BIN_LINK_TO})
+  if(ADD_BIN_DEP)
+    target_link_libraries(${ADD_BIN_TGT} ${ADD_BIN_DEP})
   endif()
 
   if(ADD_BIN_COMPILE_FLAGS)
     string(REPLACE ";" " " ADD_BIN_COMPILE_FLAGS "${ADD_BIN_COMPILE_FLAGS}")
-    set_target_properties(${ADD_BIN_TARGET_NAME} PROPERTIES
+    set_target_properties(${ADD_BIN_TGT} PROPERTIES
       COMPILE_FLAGS ${ADD_BIN_COMPILE_FLAGS})
   endif()
 
   if(ADD_BIN_LINK_FLAGS)
     string(REPLACE ";" " " ADD_BIN_LINK_FLAGS "${ADD_BIN_LINK_FLAGS}")
-    set_target_properties(${ADD_BIN_TARGET_NAME} PROPERTIES
+    set_target_properties(${ADD_BIN_TGT} PROPERTIES
       LINK_FLAGS ${ADD_BIN_LINK_FLAGS})
   endif()
 
-  if(NOT ADD_BIN_INNER)
-    install(TARGETS ${ADD_BIN_TARGET_NAME} RUNTIME
+  if(NOT ADD_BIN_PVT)
+    install(TARGETS ${ADD_BIN_TGT} RUNTIME
       DESTINATION bin
-      COMPONENT ${ADD_BIN_TARGET_NAME})
+      COMPONENT ${ADD_BIN_TGT})
   endif()
 endfunction()
